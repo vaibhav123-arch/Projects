4e4eaed35c55e312ce8948b42dcf361920deded9
vaibhav123-arch <vaibhavsinghrewa885@gmail.com>
2026-01-30 14:02:22 +0530

    completed

--- s.js
+++ s.js
@@ -1,3 +1,14 @@
+// toggle icon navbar
+
+ let menuIcon = document.querySelector('#menu-icon');
+ let navbar =document.querySelector('.navbar');
+
+ menuIcon.onclick = () => {
+ menuIcon.classList.toggle('bx-x');
+ navbar.classList.toggle('active');
+ };
+
+
 // scroll section active link
 let sections=document.querySelectorAll('section');
 let navLinks=document.querySelectorAll('header nav a');
@@ -16,4 +27,37 @@ window.onscroll=()=>{
             });
         };
     });
+    //   Sticky navbar
+    let header=document.querySelector('header');
+
+    header.classList.toggle('sticky' ,window.scrollY >100);
+
+    // remove toggle icon and navbar when click navbar link (scroll)
+     menuIcon.classList.remove('bx-x');
+     navbar.classList.remove('active');
+
 };
+
+// scroll reveal
+
+  ScrollReveal({ 
+    reset: true,
+    distance:'80px',
+    duration:1500,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content,.heading,.home-content h3,.home-content a', { origin:'top'});
+ScrollReveal().reveal('.home-image,.services-container,.portfolio-box,.contact,.contact form,.about-content p ', { origin:'bottom'});
+ScrollReveal().reveal('.home-content h1,.about-img', { origin:'left'});
+ScrollReveal().reveal('.home-content p,.about-content,.btn ', { origin:'right'});
+
+// typed js
+
+const typed= new Typed('.multiple-text',{
+    strings: ['Frontend Developer','Coder','Software Developer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
